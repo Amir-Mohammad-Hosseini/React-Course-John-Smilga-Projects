1e4a159f8696c0e4a3ff0c809400411ab4e43808
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-07-26 20:38:11 +0330

    Update routes and add new paths and elements for dashboard

--- 11-Jobster/src/routes.jsx
+++ 11-Jobster/src/routes.jsx
@@ -1,46 +1,62 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Error, Landing, Login, Register, RootLayout } from "./pages";
 import {
-  Dashboard,
-  Error,
-  Landing,
-  Login,
-  Register,
-  RootLayout,
-} from "./pages";
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 //Actions
-import {action as registerAction} from "./pages/Register"
-import {action as loginAction} from "./pages/Login"
+import { action as registerAction } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
 
-
-import store from "./store";
+import store from "./store/store";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Dashboard />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-        action : registerAction(store)
-      },
-      {
-        path: "login",
-        element: <Login />,
-        action : loginAction(store)
-      },
-      {
-        path: "landing",
-        element: <Landing />,
-      },
-    ],
-  },
+{
+  path: "/",
+  element: <RootLayout />,
+  errorElement: <Error />,
+  children: [
+    {
+      element: <SharedLayout />,
+      children: [
+        {
+          index: true,
+          element: <Stats />,
+        },
+        {
+          path: "all-jobs",
+          element: <AllJobs />,
+        },
+        {
+          path: "add-job",
+          element: <AddJob />,
+        },
+        {
+          path: "profile",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "register",
+      element: <Register />,
+      action: registerAction,
+    },
+    {
+      path: "login",
+      element: <Login />,
+      action: loginAction(store),
+    },
+    {
+      path: "landing",
+      element: <Landing />,
+    },
+  ],
+}
 ]);
 
 export default router;
