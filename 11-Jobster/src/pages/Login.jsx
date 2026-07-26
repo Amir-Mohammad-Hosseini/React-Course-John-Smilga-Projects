49667401dff70717fa09fcc820c8e0af79e03eef
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-07-26 19:20:41 +0330

    Complete logic og login page

--- 11-Jobster/src/pages/Login.jsx
+++ 11-Jobster/src/pages/Login.jsx
@@ -2,12 +2,17 @@ import Wrapper from "../assets/wrappers/RegisterPage";
 import { Logo } from "../components";
 import Button from "../components/Button";
 import InputField from "../components/InputField";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, redirect, useNavigation } from "react-router-dom";
+import { customFetch } from "../utils/axios";
+import { toast } from "react-toastify";
+import { loginUser } from "../store/features/user/userSlice";
 
 const Login = () => {
+
+  const navigation = useNavigation()
   return (
     <Wrapper className="full-page">
-      <Form className="form" method="post" >
+      <Form className="form" method="post">
         <Logo />
         <h3>Login</h3>
 
@@ -18,9 +23,12 @@ const Login = () => {
         <InputField name="password" label="password" type="password" />
 
         {/* SUBMIT BTN */}
-        <Button text="submit" />
+        <Button text={navigation.state === "submitting" ? "submitting..." : "login"} disabled={navigation.state === "submitting"} />
         <p>
-          Not a member yet? <Link className="member-btn" to="/register">Register</Link>
+          Not a member yet?{" "}
+          <Link className="member-btn" to="/register">
+            Register
+          </Link>
         </p>
       </Form>
     </Wrapper>
@@ -34,6 +42,18 @@ export const action =
   async ({ request }) => {
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
-    console.log(data, store , "Login");
+    try {
+      const response = await customFetch.post("/auth/login", data);
+      const userDatas = response.data.user;
+      store.dispatch(loginUser(userDatas))
+      toast.success(`Hello There ${userDatas.name}`);
+      return redirect("/")
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        error?.response?.data?.msg ||
+        "Please double check your credentials";
+      toast.error(errorMessage);
+    }
     return null;
   };
