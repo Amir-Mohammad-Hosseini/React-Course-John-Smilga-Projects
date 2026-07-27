5a7eb3ef80c3dbd44e6f6656dfacd677f66b37bc
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-07-27 22:51:44 +0330

    Implement update user and register user logics and connect them to the store

--- 11-Jobster/src/store/features/user/userSlice.js
+++ 11-Jobster/src/store/features/user/userSlice.js
@@ -13,7 +13,11 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    registerUser: (state, { payload }) => {},
+    registerUser: (state, { payload }) => {
+      state.user = payload
+      localStorage.setItem("user", JSON.stringify(payload));
+
+    },
     loginUser: (state, { payload }) => {
       state.user = payload;
       localStorage.setItem("user", JSON.stringify(payload));
@@ -22,9 +26,13 @@ const userSlice = createSlice({
       localStorage.removeItem("user")
       state.user = null;
     },
+    updateUserDatas : (state , {payload}) => {
+      state.user = payload.user
+      localStorage.setItem("user" , JSON.stringify(payload.user))
+    }
   },
 });
 
-export const { loginUser, logoutUser } = userSlice.actions;
+export const { registerUser ,loginUser, logoutUser , updateUserDatas } = userSlice.actions;
 
 export default userSlice.reducer;
--- 11-Jobster/src/store/store.js
+++ 11-Jobster/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import uiReducer from "./features/ui/uiSlice";
+import jobReducer from "./features/job/jobSlice";
 const store = configureStore({
   reducer: {
     userState: userReducer,
     uiState: uiReducer,
+    jobState: jobReducer,
   },
 });
 
