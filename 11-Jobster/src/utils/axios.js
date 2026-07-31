367a7fe67f6f247ce8664412fe4c35c78868e263
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-07-31 20:29:57 +0330

    outsource some utils functions(http methods and localStorage and links)

--- 11-Jobster/src/utils/axios.js
+++ 11-Jobster/src/utils/axios.js
@@ -1,6 +1,17 @@
 import axios from "axios"
+import { getUserFromLocalStorage } from "./localStorage"
 
-export const customFetch = axios.create({
+const customFetch = axios.create({
     baseURL : "https://redux-toolkit-jobster-api-server.onrender.com/api/v1"
 }
 )
+
+customFetch.interceptors.request.use((config) => {
+    const user = getUserFromLocalStorage("user")
+    if(user){
+    config.headers.Authorization = `Bearer ${user.token}`
+    }
+    return config
+})
+
+export {customFetch}
