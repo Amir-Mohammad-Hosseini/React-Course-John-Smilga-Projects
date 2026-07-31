2c0889fe8557685e00ef7bc3a9ad52e2e8656cb5
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-07-31 17:53:25 +0330

    Add Structure and logic of All-Jobs page

--- 11-Jobster/src/pages/dashboard/AllJobs.jsx
+++ 11-Jobster/src/pages/dashboard/AllJobs.jsx
@@ -1,10 +1,31 @@
+import { Suspense } from "react";
+import { jobsQuery } from "../../api/jobsQuery";
+import { SearchContainer } from "../../components";
+import { JobsContainer } from "../../components";
+import Loading from "../../components/Loading";
 
 const AllJobs = () => {
-  return (
-    <div>
-      AllJobs
-    </div>
-  )
-}
 
-export default AllJobs
+  return (
+    <>
+      <SearchContainer />
+      <Suspense fallback={<Loading center={true} />}>
+      <JobsContainer />
+      </Suspense>
+    </>
+  );
+};
+
+export default AllJobs;
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+
+    const searchParams = Object.fromEntries(url.searchParams.entries());
+    await queryClient.ensureQueryData(
+      jobsQuery({ params: searchParams }),
+    );
+    return { params: searchParams };
+  };
